c4024eaced1748a16dd4465485f124f5edaaf3b6
kenshi39847 <kenshi.t.blue@gmail.com>
2023-09-29 17:45:18 +0900

    フォームの情報を保存・エラーを表示する

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,21 @@
 class PurchasesController < ApplicationController
   def index
     @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def purchase_params
+    @item = Item.find(params[:item_id])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipalities, :street, :building, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,14 +6,14 @@ class PurchaseAddress
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :municipalities
     validates :street
-    validates :telephone_number, format: { with: /\A\d+\z/, message: "is invalid. Input only number" }
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only numbers with 10 to 11 digits" }
     validates :user_id
     validates :item_id
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, street: street, buildinge: building, telephone_number: telephone_number)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, street: street, building: building, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end
