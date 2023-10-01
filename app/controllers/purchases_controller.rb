7f52a2de28a5eaf54c6f0b7708521de0c53e86ae
kenshi39847 <kenshi.t.blue@gmail.com>
2023-10-01 17:04:57 +0900

    PAY.JPを利用したクレジットカード決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,6 @@
 class PurchasesController < ApplicationController
+  before_action :set_public_key, only: [:index, :create]
+
   def index
     @purchase_address = PurchaseAddress.new
     @item = Item.find(params[:item_id])
@@ -7,6 +9,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -14,8 +17,22 @@ class PurchasesController < ApplicationController
     end
   end
 
+  private
   def purchase_params
     @item = Item.find(params[:item_id])
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipalities, :street, :building, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipalities, :street, :building, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: @item.price, 
+      card: purchase_params[:token], 
+      currency: 'jpy' 
+    )
+  end
+
+  def set_public_key
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipalities, :street, :building, :telephone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :municipalities, :street, :building, :telephone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
@@ -9,6 +9,7 @@ class PurchaseAddress
     validates :telephone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only numbers with 10 to 11 digits" }
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
